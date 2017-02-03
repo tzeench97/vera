036dd702c2deb22965416ba74e5610d3c96c6415
tzeench <tzeench@noway.com>
2017-02-02 20:50:19 -0600

    Fixed indenting issues

--- Vera/src/tzeench97/com/vera/CustomScript_json.lua
+++ Vera/src/tzeench97/com/vera/CustomScript_json.lua
@@ -32,46 +32,43 @@ local ltn12 = require("ltn12")
 -- ================ --
 function buildAndSendJsonRequest(variableTable)
 
-	--local payload = [[ {"key":"My Key","name":"My Name","description":"The description","state":1} ]]
-	local payload = ""
-	
-	for key,value in pairs(variableTable) do 
-	
-		if payload == "" then
-			payload = key .. "===".. value
-		else
-			payload = payload .. "|||".. key .. "===".. value
-		end
+  --local payload = [[ {"key":"My Key","name":"My Name","description":"The description","state":1} ]]
+  local payload = ""
+  
+  for key,value in pairs(variableTable) do 
+    if payload == "" then
+      payload = key .. "===".. value
+    else
+      payload = payload .. "|||".. key .. "===".. value
     end
+  end
 	
-	--payload = payload .. "}} ]]"
+  --payload = payload .. "}} ]]"
+  luup.log("JSON REQUEST: "..payload)
 	
-	luup.log("JSON REQUEST: "..payload)
-	
-	--Send the payload
-	sendJSONRequest(payload)
+  --Send the payload
+  sendJSONRequest(payload)
 
 end
 
 function sendJSONRequest(jsonRequest)
-	local payload = jsonRequest
-	local response_body = { }
 
-	luup.log(" SENDING JSON ")
-	local res, code, response_headers, status = http.request
-	{
-		url = JSON_URL,
-		method = "POST",
-		headers =
-		{
-			["Authorization"] = "Maybe you need an Authorization header?", 
-			["Content-Type"] = "application/json",
-			["Content-Length"] = payload:len()
-		},
-		source = ltn12.source.string(payload),
-		sink = ltn12.sink.table(response_body)
-	}
-	--luup.task('Response: = ' .. table.concat(response_body) .. ' code = ' .. code .. '   status = ' .. status,1,'Sample POST request with JSON data',-1)
-	luup.log(" SENDING JSON COMPLETED ")
+  local payload = jsonRequest
+  local response_body = { }
+  
+  luup.log(" SENDING JSON ")
+  local res, code, response_headers, status = http.request {
+        url = JSON_URL,
+        method = "POST",
+        headers = {
+                    ["Authorization"] = "Maybe you need an Authorization header?", 
+                    ["Content-Type"] = "application/json",
+                    ["Content-Length"] = payload:len()
+                  },
+        source = ltn12.source.string(payload),
+        sink = ltn12.sink.table(response_body)
+  }
+  --luup.task('Response: = ' .. table.concat(response_body) .. ' code = ' .. code .. '   status = ' .. status,1,'Sample POST request with JSON data',-1)
+  luup.log(" SENDING JSON COMPLETED ")
 	
 end
--- Vera/src/tzeench97/com/vera/CustomScript_pushover.lua
+++ Vera/src/tzeench97/com/vera/CustomScript_pushover.lua
@@ -55,65 +55,65 @@ Usage Syntax:
 -- PUBLIC FUNCTIONS --
 -- ================ --
 function pushover(request)
-	luup.log("=======================================================================")
-	luup.log("                         PUSHOVER REQUEST                              ")
-	luup.log("=======================================================================")
-	
-	local http = require("socket.http")
-	local pushoverUrl = "https://api.pushover.net/1/messages.json"
-	http.TIMEOUT = 5
-	
-	-- Build the POST string
-	local data_str = {}
-	table.insert(data_str,"token=" .. tostring(PUSHOVER_TOKEN))
-	table.insert(data_str,"user=" .. tostring(PUSHOVER_USERKEY))
-	for k,v in pairs(request) do
-		table.insert(data_str, tostring(k) .. "=" .. tostring(v))
-	end
-	data_str = table.concat(data_str, "&")
-	
-	-- For Debugging
-	luup.log("VARIABLE VALUES")
-	luup.log("token: " .. tostring(PUSHOVER_TOKEN))
-	luup.log("user: " .. tostring(PUSHOVER_USERKEY))
-	for k,v in pairs(request) do
-		luup.log(tostring(k) .. ": " .. tostring(v))
-	end
-	luup.log("============================================================")
-	luup.log("Data Sent to PushOver: " .. data_str)
-	luup.log("============================================================")
-	
-	-- Send the request and collect the result
-	local res, code, headers, status = http.request(pushoverUrl,data_str)
-	
-	-- Check for errors
-	if (code ~= 200) then
-		local errstr = "Error while sending request. Status code: " .. tostring(code) .. ", Body: " .. tostring(res)
-		return false, errstr
-	else 
-		return true, "OK"
-	end
-	
-	luup.log("=======================================================================")
-	luup.log("                      PUSHOVER REQUEST (DONE)                          ")
-	luup.log("=======================================================================")
-	luup.log("                                                                       ")
+  luup.log("=======================================================================")
+  luup.log("                         PUSHOVER REQUEST                              ")
+  luup.log("=======================================================================")
+  
+  local http = require("socket.http")
+  local pushoverUrl = "https://api.pushover.net/1/messages.json"
+  http.TIMEOUT = 5
+  
+  -- Build the POST string
+  local data_str = {}
+  table.insert(data_str,"token=" .. tostring(PUSHOVER_TOKEN))
+  table.insert(data_str,"user=" .. tostring(PUSHOVER_USERKEY))
+  for k,v in pairs(request) do
+    table.insert(data_str, tostring(k) .. "=" .. tostring(v))
+  end
+  data_str = table.concat(data_str, "&")
+  
+  -- For Debugging
+  luup.log("VARIABLE VALUES")
+  luup.log("token: " .. tostring(PUSHOVER_TOKEN))
+  luup.log("user: " .. tostring(PUSHOVER_USERKEY))
+  for k,v in pairs(request) do
+    luup.log(tostring(k) .. ": " .. tostring(v))
+  end
+  luup.log("============================================================")
+  luup.log("Data Sent to PushOver: " .. data_str)
+  luup.log("============================================================")
+  
+  -- Send the request and collect the result
+  local res, code, headers, status = http.request(pushoverUrl,data_str)
+  
+  -- Check for errors
+  if (code ~= 200) then
+    local errstr = "Error while sending request. Status code: " .. tostring(code) .. ", Body: " .. tostring(res)
+    return false, errstr
+  else 
+    return true, "OK"
+  end
+  
+  luup.log("=======================================================================")
+  luup.log("                      PUSHOVER REQUEST (DONE)                          ")
+  luup.log("=======================================================================")
+  luup.log("                                                                       ")
 end
 
 function logPushoverResponse(success, err)
-	luup.log("=======================================================================")
-	luup.log("                          PUSHOVER RESPONSE                            ")
-	luup.log("=======================================================================")
-	luup.log("VARIABLE VALUES")
-	luup.log("success: " .. tostring(success))
-	luup.log("err:" .. tostring(err))
-	if (success) then
-	  luup.log("Hells yeah! The push was sent successfully.")
-	else
-	  luup.log("Awe snap! The push failed with error: " .. err)
-	end
-	luup.log("=======================================================================")
-	luup.log("                      PUSHOVER RESPONSE (DONE)                         ")
-	luup.log("=======================================================================")
-	luup.log("                                                                       ")
+  luup.log("=======================================================================")
+  luup.log("                          PUSHOVER RESPONSE                            ")
+  luup.log("=======================================================================")
+  luup.log("VARIABLE VALUES")
+  luup.log("success: " .. tostring(success))
+  luup.log("err:" .. tostring(err))
+  if (success) then
+    luup.log("Hells yeah! The push was sent successfully.")
+  else
+    luup.log("Awe snap! The push failed with error: " .. err)
+  end
+  luup.log("=======================================================================")
+  luup.log("                      PUSHOVER RESPONSE (DONE)                         ")
+  luup.log("=======================================================================")
+  luup.log("                                                                       ")
 end
