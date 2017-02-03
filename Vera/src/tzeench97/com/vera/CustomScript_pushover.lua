3761f68b50fbb5ed347d1f7a7310d887ea3aa2d8
tzeench <tzeench@noway.com>
2017-02-02 19:05:57 -0600

    Initial check-in

---
+++ Vera/src/tzeench97/com/vera/CustomScript_pushover.lua
@@ -0,0 +1,69 @@
+-- Usage: success (boolean), err (string) = pushover( { a table with keys and values corresponding to pushover API } )
+function pushover(request)
+	luup.log("=======================================================================")
+	luup.log("                         PUSHOVER REQUEST                              ")
+	luup.log("=======================================================================")
+	
+	local http = require("socket.http")
+	local pushoverUrl = "https://api.pushover.net/1/messages.json"
+	http.TIMEOUT = 5
+	
+	-- Build the POST string
+	local data_str = {}
+	table.insert(data_str,"token=" .. tostring(PUSHOVER_TOKEN))
+	table.insert(data_str,"user=" .. tostring(PUSHOVER_USERKEY))
+	for k,v in pairs(request) do
+		table.insert(data_str, tostring(k) .. "=" .. tostring(v))
+	end
+	data_str = table.concat(data_str, "&")
+	
+	-- For Debugging
+	luup.log("VARIABLE VALUES")
+	luup.log("token: " .. tostring(PUSHOVER_TOKEN))
+	luup.log("user: " .. tostring(PUSHOVER_USERKEY))
+	for k,v in pairs(request) do
+		luup.log(tostring(k) .. ": " .. tostring(v))
+	end
+	luup.log("------------------------------------------------------------")
+	luup.log("Data Sent to PushOver: " .. data_str)
+	luup.log("============================================================")
+	
+	-- Send the request and collect the result
+	local res, code, headers, status = http.request(pushoverUrl,data_str)
+	
+	-- Check for errors
+	if (code ~= 200) then
+		local errstr = "Error while sending request. Status code: " .. tostring(code) .. ", Body: " .. tostring(res)
+		return false, errstr
+	else 
+		return true, "OK"
+	--[[
+	elseif (res ~= '{"status":1}') then
+		local errstr = "Error from pushover: " .. tostring(res)
+		return false, errstr
+	]]--
+	end
+	
+	luup.log("=======================================================================")
+	luup.log("                      PUSHOVER REQUEST (DONE)                          ")
+	luup.log("=======================================================================")
+	luup.log("                                                                       ")
+end
+
+function logPushoverResponse(success, err)
+	luup.log("=======================================================================")
+	luup.log("                          PUSHOVER RESPONSE                            ")
+	luup.log("=======================================================================")
+	luup.log("VARIABLE VALUES")
+	luup.log("success: " .. tostring(success))
+	luup.log("err:" .. tostring(err))
+	if (success) then
+	  luup.log("Hells yeah! The push was sent successfully.")
+	else
+	  luup.log("Awe snap! The push failed with error: " .. err)
+	end
+	luup.log("=======================================================================")
+	luup.log("                      PUSHOVER RESPONSE (DONE)                         ")
+	luup.log("=======================================================================")
+	luup.log("                                                                       ")
+end
